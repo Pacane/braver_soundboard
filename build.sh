b5893fa47468a07e12d14997ee42268ae6b6b75b
Joel Trottier-Hebert <iamquebecois@gmail.com>
2022-07-26 20:46:15 -0400

    fix build script

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 ls lavalink/assets > assets.txt
 
 docker build --platform=linux/amd64 -t braver-bot:latest-amd64 -f Dockerfile .
-docker tag braver-bot northamerica-northeast1-docker.pkg.dev/stacktrace-295302/pacane/braver-bot:latest-amd64
-docker push northamerica-northeast1-docker.pkg.dev/stacktrace-295302/pacane/braver-bot-amd64
+docker tag braver-bot:latest-amd64 northamerica-northeast1-docker.pkg.dev/stacktrace-295302/pacane/braver-bot:latest-amd64
+docker push northamerica-northeast1-docker.pkg.dev/stacktrace-295302/pacane/braver-bot:latest-amd64
